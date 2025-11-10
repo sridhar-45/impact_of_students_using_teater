4368ee7d4edcce059269f3982f56cb96c1be1023
sridhar goudu <sridhargoudu7@gmail.com>
2025-11-10 17:07:37 +0530

    Update teater_impact_of_students.py

--- teater_impact_of_students.py
+++ teater_impact_of_students.py
@@ -31,7 +31,7 @@ from functools import reduce
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306)
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
 
